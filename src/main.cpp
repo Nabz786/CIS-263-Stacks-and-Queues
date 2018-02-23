bc10d9f3e970cfbcc5011e521a4e8742a97c18c0
Nabeel Vali <valin@eos24.cis.gvsu.edu>
2018-02-23 13:07:27 -0500

    passed string into file then passed into checkBal()

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "functions.h"
 #include "iostream"
+#include "string"
 int main(int argc, char** argv){
 	
 		//test class is in a textfile called test
-		balancedSymbols(loadFile("test"));
+		//balancedSymbols(loadFile("test"));
 	
+		std::string inputString = loadFile("file");
+		balancedSymbols(inputString);
 		std::cout << iToP("1+2*3+6") << std::endl;
 		std::cout << iToP("2+8*3+(7+6*6)*5+(6*3-2)/7") << std::endl;
 		std::cout << iToP("(5*2)*((7-5*2)*(5+3))") << std::endl;
