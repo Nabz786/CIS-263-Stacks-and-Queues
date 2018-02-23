2f209c197584c722b9fb8e5362a1baeedc6c921a
Nabeel Vali <valin@eos25.cis.gvsu.edu>
2018-02-23 01:52:49 -0500

    Updated test

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,5 @@ int main(int argc, char** argv){
 	
 		std::cout << iToP("1+2*3+6") << std::endl;
 		std::cout << iToP("2+8*3+(7+6*6)*5+(6*3-2)/7") << std::endl;
-		std::cout << iToP("2*2/(6*3(5*7)+6))") << std::endl;
+		std::cout << iToP("(5*2)*((7-5*2)*(5+3))") << std::endl;
 	}
